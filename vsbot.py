1240c465af2a3303693a12b8154d10aa97aacb52
Pragati-1904 <112616265+Pragati-1904@users.noreply.github.com>
2023-01-12 18:24:38 +0530

    Update vsbot.py

--- vsbot.py
+++ vsbot.py
@@ -75,7 +75,7 @@ async def main_task(
             for dest in dests:
                 try:
                     await msg.forward_to(dest)
-                    await asyncio.sleep(1)
+                    await asyncio.sleep(120)
                     f += 1
                 except BaseException:
                     pass
